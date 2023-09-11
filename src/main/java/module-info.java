8d8c0a693bcffbeb27394512fe18b3e6af43cd43
JUKOMU <118241067+JUKOMU@users.noreply.github.com>
2023-09-11 14:14:34 +0800

    更新JavaCV和条形码的依赖

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,8 @@ open module LibraryManageSystem {
     requires druid;
     requires java.datatransfer;
     requires java.desktop;
+    requires com.google.zxing;
+    requires com.google.zxing.javase;
+    requires org.bytedeco.javacv;
+    requires javafx.swing;
 }
