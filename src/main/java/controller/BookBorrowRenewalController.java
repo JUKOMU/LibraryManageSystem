7b460e48fcef4797533eeffd3a914d0825d535ac
JUKOMU <13430844+jukomu@user.noreply.gitee.com>
2023-09-13 10:45:40 +0800

    优化代码结构

--- src/main/java/controller/BookBorrowRenewalController.java
+++ src/main/java/controller/BookBorrowRenewalController.java
@@ -1,27 +1,18 @@
 package controller;
 
-import com.google.zxing.*;
-import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
-import com.google.zxing.common.HybridBinarizer;
 import domain.Book;
-import domain.User;
 import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.Java2DFrameConverter;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
 import service.BookService;
-import service.UserService;
 import service.impl.BookServiceImpl;
-import service.impl.UserServiceImpl;
 import utils.Result;
 import zxing.BarCode;
 
@@ -31,8 +22,6 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.URL;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -183,62 +172,55 @@ public class BookBorrowRenewalController {
             try {
                 doExecuteFrame(grabber.grab(), "src/main/resources/img/1.jpg");
                 num = getBarCodePos();
-                try {
-                    // 读取PNG图片
-                    BufferedImage image2 = ImageIO.read(new File("E:\\Java\\LibraryManageSystem\\src\\main\\resources\\img\\barPos.png"));
 
-                    // 创建Graphics2D对象以绘制在图像上
-                    Graphics2D g2d = image2.createGraphics();
+                // 读取PNG图片
+                BufferedImage image2 = ImageIO.read(new File("E:\\Java\\LibraryManageSystem\\src\\main\\resources\\img\\barPos.png"));
 
-                    // 设置线条颜色和宽度
-                    g2d.setColor(Color.GREEN);
-                    g2d.setStroke(new BasicStroke(2)); // 设置线条宽度
+                // 创建Graphics2D对象以绘制在图像上
+                Graphics2D g2d = image2.createGraphics();
 
-                    // 绘制线条
-                    for (int i = 0; i < 4; i++) {
-                        if (i < 3) {
-                            g2d.drawLine(num[i][0], num[i][1], num[i + 1][0], num[i + 1][1]);
-                        } else {
-                            g2d.drawLine(num[i][0], num[i][1], num[0][0], num[0][1]);
-                        }
+                // 设置线条颜色和宽度
+                g2d.setColor(Color.GREEN);
+                g2d.setStroke(new BasicStroke(2)); // 设置线条宽度
+
+                // 绘制线条
+                for (int i = 0; i < 4; i++) {
+                    if (i < 3) {
+                        g2d.drawLine(num[i][0], num[i][1], num[i + 1][0], num[i + 1][1]);
+                    } else {
+                        g2d.drawLine(num[i][0], num[i][1], num[0][0], num[0][1]);
                     }
+                }
 
-                    // 释放Graphics2D对象
-                    g2d.dispose();
+                // 释放Graphics2D对象
+                g2d.dispose();
 
-                    // 保存修改后的图像
-                    ImageIO.write(image2, "PNG", new File("E:\\Java\\LibraryManageSystem\\src\\main\\resources\\img\\output.png"));
+                // 保存修改后的图像
+                ImageIO.write(image2, "PNG", new File("E:\\Java\\LibraryManageSystem\\src\\main\\resources\\img\\output.png"));
 
-                    System.out.println("线条已绘制到图像上并保存成功！");
-                    Image img = new Image("E:\\Java\\LibraryManageSystem\\src\\main\\resources\\img\\output.png");
-                    camera2.setImage(img);
+                System.out.println("线条已绘制到图像上并保存成功！");
+                Image img = new Image("E:\\Java\\LibraryManageSystem\\src\\main\\resources\\img\\output.png");
+                camera2.setImage(img);
 
-                    String code = BarCode.readCode(new File("E:\\Java\\LibraryManageSystem\\src\\main\\resources\\img\\1.jpg"));
-                    if (code != null && !isRecognized) {
-                        isRecognized = true;
-                        BookService bookService = new BookServiceImpl();
+                String code = BarCode.readCode(new File("E:\\Java\\LibraryManageSystem\\src\\main\\resources\\img\\1.jpg"));
+                if (code != null && !isRecognized) {
+                    isRecognized = true;
+                    BookService bookService = new BookServiceImpl();
 
-                        Book book = new Book();
-                        book.setBarcode(code);
-                        try {
-                            Result result = bookService.findBookByBarCode(book);
-                            if (result.getCode() == 200) {
-                                System.out.println("ok");
-                                //正确
+                    Book book = new Book();
+                    book.setBarcode(code);
+                    Result result = bookService.findBookByBarCode(book);
+                    if (result.getCode() == 200) {
+                        System.out.println("ok");
+                        //正确
 
-                                Book book2 = (Book) result.getData();
-                                book_name.setText(book2.getName());
-                                book_author.setText(book2.getAuthor());
-                                Image image3 = new Image("E:\\Java\\LibraryManageSystem\\src\\main\\resources\\img\\database_source\\" + book2.getId() + ".jpg");
-                                book_view.setImage(image3);
-                                success.setText("借书成功");
-                            }else {isRecognized = false;}
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+                        Book book2 = (Book) result.getData();
+                        book_name.setText(book2.getName());
+                        book_author.setText(book2.getAuthor());
+                        Image image3 = new Image("E:\\Java\\LibraryManageSystem\\src\\main\\resources\\img\\database_source\\" + book2.getId() + ".jpg");
+                        book_view.setImage(image3);
+                        success.setText("借书成功");
+                    }else {isRecognized = false;}
                 }
             } catch (IOException e) {
                 e.printStackTrace();
